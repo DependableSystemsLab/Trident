e65802eab99a7e61c5c61d1d870d1b630db0696a
Justin <cclinus@hotmail.com>
2018-05-25 16:05:28 -0700

    clean up

--- inst_count/run_sampling.sh
+++ inst_count/run_sampling.sh
@@ -1,4 +1,5 @@
 ~/llvm-2.9-build/bin/opt -S -load ~/llvm-2.9-build/lib/InstSampling.so -bishe_insert blackscholes-llfi_index.ll -o blackscholes_profile.ll
+llvm-gcc -emit-llvm -S sampling.c -o sampling.ll
 llvm-link blackscholes_profile.ll sampling.ll -o blackscholes_linked.ll
 rm output.txt instCountResult.txt fi_breakdown.txt
 lli blackscholes_linked.ll 1 in_4.txt output.txt
