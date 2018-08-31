74dcd8245dd8be0072ab025c607db21ab70ac826
abanwer <arehman.anwe1@gmail.com>
2018-08-31 16:09:32 -0700

    Updating documentation and implementation of updating paths in configuration fils

--- test/config.py
+++ test/config.py
@@ -5,8 +5,8 @@ LLVMGCC="llvm-gcc"
 ROOT = ""
 
 # Update the paths according to your environment
-OPT= ROOT + "llvm-2.9-build/bin/opt"  
-LLVMLINK= ROOT + "llvm-2.9-build/bin/llvm-link" 
-LLVMPASS_FOLDER= ROOT + "llvm-2.9-build/lib/" 
+OPT= ROOT + "/llvm-2.9-build/bin/opt"  
+LLVMLINK= ROOT + "/llvm-2.9-build/bin/llvm-link" 
+LLVMPASS_FOLDER= ROOT + "/llvm-2.9-build/lib/" 
 LLI= ROOT + "/llvm-2.9-build/bin/lli"
 PYTHON="python"
